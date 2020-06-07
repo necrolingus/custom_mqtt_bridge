cd0f9ed73780693386c017aba67b604c5ea2168f
necrolingus <leigh.williams88@gmail.com>
2020-06-07 08:28:46 +0200

    removed stuff

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const mqtt = require('mqtt')
 
 const connect_options_remote_main = {
     clientId:"custom_bridge_0001",
-    username:"tester",
-    password:"SuperLongPassHallo2@",
+    username:"xxx",
+    password:"xxxx",
     clean:false, //this is a persistent connection.
     rejectUnauthorized : false,
     reconnectPeriod: 5000
@@ -17,16 +17,16 @@ const connect_options_remote_main = {
 
 const connect_options_remote_stat = {
     clientId:"custom_bridge_0002",
-    username:"tester",
-    password:"SuperLongPassHallo2@",
+    username:"xxxx",
+    password:"xxxx",
     clean:false, //this is a persistent connection.
     rejectUnauthorized : false
 }
 
 const connect_options_local = {
     clientId:"custom_bridge_0001",
-    username:"test",
-    password:"test",
+    username:"xxx",
+    password:"xxx",
     clean:false, //this is a persistent connection.
 }
 
@@ -38,14 +38,14 @@ var publish_options = {
 var topic_list = ["cmnd/sonoff/dining-lamp/POWER"]
 
 
-const client_remote = mqtt.connect('mqtts://94-237-53-21.uk-lon1.upcloud.host:8883', connect_options_remote_main)
+const client_remote = mqtt.connect('mqtts://9xxxxx:8883', connect_options_remote_main)
 client_remote.on("connect", () => {	
     console.log("Main Remote Connected: " + client_remote.connected)
 
     client_remote.subscribe(topic_list) //only subscribe to cmnd topics here
     client_remote.on('message', (topic, message, packet) => {
         
-        const client_local = mqtt.connect('mqtt://192.168.88.110:1883', connect_options_local)
+        const client_local = mqtt.connect('mqtt://xxxx:1883', connect_options_local)
         client_local.on("connect", () => {	
             console.log("Main Local Connected: " + client_remote.connected)
             client_local.publish(topic, message, publish_options)
@@ -60,7 +60,7 @@ client_remote.on("connect", () => {
                 console.log("Main Local Topic: " + topic)
 
                 //lets push that STAT message to the remote server and disconnect
-                const client_remote2 = mqtt.connect('mqtts://94-237-53-21.uk-lon1.upcloud.host:8883', connect_options_remote_stat)
+                const client_remote2 = mqtt.connect('mqtts://xxxx:8883', connect_options_remote_stat)
                 client_remote2.on("connect", () => {
                     console.log("Secondary Remote Connected: " + client_remote2.connected)	
                     client_remote2.publish(topic, message, publish_options)
